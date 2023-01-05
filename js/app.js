2483d0fd324db10615b028b304fee4f9198c3a87
FrancoEzequielRivera <84022619+FrancoEzequielRivera@users.noreply.github.com>
2023-01-05 16:30:42 -0300

    Mejoras
    Mejoré redes sociales y agregué botón de subir arriba de todo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-console.log("Hola mundo!")
+console.log("Hola mundo!");
+
+
+const ir_arriba = document.getElementById('arriba')
+
+ir_arriba.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+})
+
+window.onscroll = () => {
+    if (window.scrollY > 0)
+    {
+        ir_arriba.style.display = "inline-block"
+        ir_arriba.style.opacity = "1"
+    }
+    else
+    {
+        ir_arriba.style.opacity = "0"
+        setTimeout(() =>{
+            ir_arriba.style.display = "none";
+        },300)
+    }
+}
